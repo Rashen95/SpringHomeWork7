abf005347e7c4ecf18efa7bc5090fe4693ff438a
Artem Privalov <rashen95@mail.ru>
2024-02-10 14:18:49 +0300

    initial commit

---
+++ src/main/java/ru/geekbrains/SpringHomeWork7/SpringHomeWork7Application.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.SpringHomeWork7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeWork7Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomeWork7Application.class, args);
+    }
+}
---
+++ src/test/java/ru/geekbrains/SpringHomeWork7/SpringHomeWork7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringHomeWork7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWork7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
